30ed25ee48b49b51c03579e06240aee128c8a481
Ivar Grytten <ivar@1337.no>
2016-11-08 13:58:02 +0100

    tests

---
+++ test/testSequences.py
@@ -0,0 +1,13 @@
+import unittest
+from sequences import get_sequence
+
+
+class TestSequences(unittest.TestCase):
+    def get_ucsc_sequence(self):
+        seq = get_sequence("chr11_KI270831v1_alt", 400, 410)
+        correct = "gtcttttttct"
+        self.assertEqual(correct, seq, "%s is not equal to the sequence fetched from ucsc (%s" % (correct, seq))
+
+
+if __name__ == "__main__":
+    unittest.main()
